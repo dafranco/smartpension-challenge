66ee3a1d74920e388f9376bbcf4b4bf3f4a5bfe2
David Franco <francodavid20@gmail.com>
2021-10-25 08:35:47 -0300

    Change more var names for readability

--- lib/unique_visits_counter.rb
+++ lib/unique_visits_counter.rb
@@ -8,10 +8,10 @@ class UniqueVisitsCounter < LogCounter
   def group_by_count
     results = {}
     @log_records.group_by(&:ip)
-                .select { |_k, visits| visits.size == 1 }
-                .each do |_k, v|
-                  results[v.first.page] ||= 0
-                  results[v.first.page] += 1
+                .select { |_ip, visits| visits.size == 1 }
+                .each do |_ip, visits|
+                  results[visits.first.page] ||= 0
+                  results[visits.first.page] += 1
                 end
     results
   end
